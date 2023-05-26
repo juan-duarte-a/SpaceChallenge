edbd3d8160e692e8bc346c6c21b32914da74f775
Juan Duarte <juan.duarte.a@gmail.com>
2023-05-26 00:02:58 -0500

    Initial commit

---
+++ src/spacechallenge/SpaceChallenge.java
@@ -0,0 +1,17 @@
+package spacechallenge;
+
+/**
+ * Java final project from the Java Object Oriented Programming course.
+ * 
+ * @author juan
+ */
+public class SpaceChallenge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
